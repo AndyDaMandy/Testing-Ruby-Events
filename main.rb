d7973f28dfc621a813c2b3c7612f645e1b894a25
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-05-21 11:27:45 -0400

    requires CSV

--- main.rb
+++ main.rb
@@ -1,12 +1,10 @@
+require 'csv'
 puts 'Event Manager Initialized!'
 #puts File.exist? "event_attendees.csv"
 #contents = File.read('event_attendees.csv')
 #puts contents
-
-lines = File.readlines('event_attendees.csv')
-lines.each do |line|
-  columns = line.split(",")
-  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-  name = columns[3] + ", " + columns[2]
+contents = CSV.open('event_attendees.csv')
+contents.each do |row|
+  name = row[2]
   puts name
 end
